b518507561e00e329625440fbc2221a74c16c1f5
Oguzhan Uysal <development@oguzhanuysal.eu>
2019-12-07 20:35:54 +0100

    Split messages because it causes weird behaviour in consumers

--- src/Message/PersistVideoStatusMessage.php
+++ src/Message/PersistVideoStatusMessage.php
@@ -4,12 +4,58 @@
 namespace Pbxg33k\MessagePack\Message;
 
 
-class PersistVideoStatusMessage extends VideoCheckedMessage
+class PersistVideoStatusMessage extends FileMessage
 {
+    /**
+     * @var boolean
+     */
+    private $checked;
+
+    /**
+     * @var boolean
+     */
+    private $consistent;
+
     public function __construct(string $path, bool $checked, bool $consistent)
     {
         parent::__construct($path);
         $this->setChecked($checked);
         $this->setConsistent($consistent);
+    }
+
+    /**
+     * @return bool
+     */
+    public function isChecked(): bool
+    {
+        return $this->checked;
+    }
+
+    /**
+     * @param bool $checked
+     * @return VideoCheckedMessage
+     */
+    public function setChecked(bool $checked): VideoCheckedMessage
+    {
+        $this->checked = $checked;
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isConsistent(): bool
+    {
+        return $this->consistent;
+    }
+
+    /**
+     * @param bool $consistent
+     * @return VideoCheckedMessage
+     */
+    public function setConsistent(bool $consistent): VideoCheckedMessage
+    {
+        $this->consistent = $consistent;
+        return $this;
     }
 }
